9ad588b7b2469a1c3d4234dece417c807e7d0e1b
CamilaDomatoITR <Camila_Domato@itrsa.com.ar>
2024-02-15 19:45:24 -0300

    Add project files.

---
+++ HomeBankingMindHub/Models/DBInitializer.cs
@@ -0,0 +1,30 @@
+﻿namespace HomeBankingMindHub.Models
+{
+    public class DBInitializer
+    {
+        public static void Initialize(HomeBankingContext context)
+        {
+            if (!context.Clients.Any())
+            {
+                var clients = new Client[]
+                {
+                    new Client { Email = "vcoronado@gmail.com",
+                        FirstName="Victor", LastName="Coronado",
+                        Password="123456"},
+                    new Client { Email = "cdomato@gmail.com",
+                        FirstName="Camila", LastName="Domato",
+                        Password="112233"},
+                    new Client { Email = "dbenitez@gmail.com",
+                        FirstName="Damian", LastName="Benitez",
+                        Password="7891011"}
+                };
+
+                context.Clients.AddRange(clients);
+
+                //guardamos
+                context.SaveChanges();
+            }
+
+        }
+    }
+}
